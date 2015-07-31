c3bb755b1ef518c975499b082f62d75510e9244f
DHM <username@users.noreply.github.com>
2015-07-31 10:23:24 -0700

    Updates to Capsule NAmes

--- Code/JoinCapsuleNames.R
+++ Code/JoinCapsuleNames.R
@@ -2,6 +2,7 @@
 ###
 #setwd("Code")
 CLtrain <- read.csv("../Data/coupon_list_train.csv", stringsAsFactors=F)
+CLtest <- read.csv("../Data/coupon_list_test.csv", stringsAsFactors=F)
 capsule <- read.csv("../Data/capsule.csv")
 cap <- table(CLtrain$CAPSULE_TEXT)
 cap[grep("^WEB",names(cap))]<- 43
@@ -10,7 +11,6 @@ capsule <- cbind(capsule, names(cap), cap)
 caps <- capsule[,c(2,3)]
 names(caps)<- c("Eng","CAPSULE_TEXT")
 CLtrain <- merge(CLtrain, caps, by="CAPSULE_TEXT")
-rownames(capsule) <- capsule$English.Translation
-names(capsule$cap) <- capsule$English.Translation
-save(CLtrain,file="../Data/CLtrain.Rdata")
+CLtest <- merge(CLtest, caps, by="CAPSULE_TEXT")
+save(CLtrain,CLtest,file="../Data/CLwithEng.Rdata")
 
